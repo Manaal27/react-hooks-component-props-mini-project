8692020c1b9d9d6c9e764cc2d3ac36b8c1780ce3
manaal <manaal.basabra@student.moringaschool.com>
2022-09-24 13:40:35 +0300

    done

---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}) {
+    const article = posts.map((article)=> (
+        <Article 
+        key={article.id}
+        title={article.title}
+        date={article.date}
+        preview={article.preview}
+        minutes={article.minutes}
+        />
+    ))
+
+    return <main>{article}</main>
+}
+
+export default ArticleList;
